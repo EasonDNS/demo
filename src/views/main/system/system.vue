9c7ecb0dd215a0e66a6d8f260d4f58e7cf10b19f
张真 <zzsonyzz12@163.com>
2021-11-24 22:18:39 +0800

    添加了一个syste

---
+++ src/views/main/system/system.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="system">
+    {{msg}}
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref, reactive, computed } from 'vue'
+
+export default defineComponent({
+  components: {},
+  setup() {
+    const msg = ref('msg in app')
+    return {}
+  }
+})
+</script>
+<style lang="less" scoped></style>
