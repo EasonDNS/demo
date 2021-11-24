1701bbdb9585b9b5cdd55051151ddfebc83d5e61
easonDNS <zzsonyzz12@hotmail.com>
2021-11-24 08:39:45 +0800

    第一次提交 初始化

---
+++ src/views/login/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login"></div>
+</template>
+<script lang="ts">
+import { defineComponent, ref, reactive, computed } from 'vue'
+
+export default defineComponent({
+  components: {},
+  setup() {
+    const msg = ref('msg in app')
+    return {}
+  }
+})
+</script>
+<style lang="less" scoped></style>
